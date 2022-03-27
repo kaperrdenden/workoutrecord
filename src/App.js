0f30af609c17c4705e9dc8734ef1c946752c06a8
kaperrdenden <weather2sogood@gmail.com>
2022-03-27 23:54:24 +0900

    ListForExercise 렌더링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
+import ListForExercise from './components/ListForExercise';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <ListForExercise />
     </div>
   );
 }
--- src/components/ListForExercise.js
+++ src/components/ListForExercise.js
@@ -1,6 +1,8 @@
 const ListForExercise = () => {
     return(
-
+        <ul>
+            dsf
+        </ul>
     )
 }
 
